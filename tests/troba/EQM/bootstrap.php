f1cda7c669e9cc4a4bbf6402e3552aedd6d75885
Tobias Deekens <nerd@tdeekens.name>
2013-10-18 15:16:08 +0200

    Changes test username and password for travis.

--- tests/troba/EQM/bootstrap.php
+++ tests/troba/EQM/bootstrap.php
@@ -14,16 +14,16 @@ use troba\EQM\ClassicConventionHandler;
 # default
 EQM::initialize([
     'dsn' => 'mysql:host=localhost;dbname=orm_test',
-    'username' => 'root',
-    'password' => 'boot',
+    'username' => 'travis',
+    'password' => '',
     EQM::RUN_MODE => EQM::DEV_MODE
 ]);
 
 # second_db
 EQM::initialize([
     'dsn' => 'mysql:host=localhost;dbname=orm_test2',
-    'username' => 'root',
-    'password' => 'boot',
+    'username' => 'travis',
+    'password' => '',
     EQM::CONVENTION_HANDLER => new ClassicConventionHandler(),
     EQM::RUN_MODE => EQM::DEV_MODE
 ], 'second_db');
